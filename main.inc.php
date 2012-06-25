18c847ea24e0f1e13e161aa67c6ebe2bab3cf653
xbgmsharp <xbgmsharp@gmail.com>
2012-06-25 14:49:59 -0400

    Fixed: Tabulation layout

--- main.inc.php
+++ main.inc.php
@@ -38,142 +38,142 @@ add_event_handler('get_admin_plugin_menu_links', 'videojs_admin_menu' );
 
 function videojs_admin_menu($menu)
 {
-  array_push($menu,
-      array(
-        'NAME' => 'videojs',
-        'URL'  => get_admin_plugin_menu_link(dirname(__FILE__).'/admin.php')
-      )
-    );
-  return $menu;
+	array_push($menu,
+		array(
+			'NAME' => 'videojs',
+			'URL'  => get_admin_plugin_menu_link(dirname(__FILE__).'/admin.php')
+		)
+	);
+	return $menu;
 }
 
 function render_media($content, $picture)
 {
-    global $template, $picture, $page, $conf, $user, $refresh;
+	global $template, $picture, $page, $conf, $user, $refresh;
 	//print_r( $picture['current']);
-    // do nothing if the current picture is actually an image !
+	// do nothing if the current picture is actually an image !
 	// but doesn't not work ???
-    if ( @$picture['current']['is_picture'] )
-    {
+	if ( @$picture['current']['is_picture'] )
+	{
 		return $content;
-    }
+	}
 
-    // In case, the we handle a large video, we define a MAX_WIDTH
-    // variable to limit the display size.
-    if (isset($user['maxwidth']) and $user['maxwidth']!='')
-    {
-        $MAX_WIDTH = $user['maxwidth'];
-    }
-    else
-    {
-        $MAX_WIDTH = '720';
-    }
+	// In case, the we handle a large video, we define a MAX_WIDTH
+	// variable to limit the display size.
+	if (isset($user['maxwidth']) and $user['maxwidth']!='')
+	{
+		$MAX_WIDTH = $user['maxwidth'];
+	}
+	else
+	{
+		$MAX_WIDTH = '720';
+	}
 	//print "$MAX_WIDTH=" . $MAX_WIDTH;
 	//print_r($user);
 
-    // Get video infos with getID3 lib
-    require_once(dirname(__FILE__) . '/include/getid3/getid3.php');
-    $getID3 = new getID3;
-    $fileinfo = $getID3->analyze($picture['current']['path']);
+	// Get video infos with getID3 lib
+	require_once(dirname(__FILE__) . '/include/getid3/getid3.php');
+	$getID3 = new getID3;
+	$fileinfo = $getID3->analyze($picture['current']['path']);
 	//print "getID3\n";
 	//print_r($fileinfo);
 
-    $extension = strtolower(get_extension($picture['current']['path']));
+	$extension = strtolower(get_extension($picture['current']['path']));
 	//print "extension\n";
 	//print_r($extension);
 
-    if(isset($fileinfo['video']))
+	if(isset($fileinfo['video']))
 	{
-        // -- video file --
-        // guess resolution
-        if (isset($fileinfo['video']['resolution_x']) )
+		// -- video file --
+		// guess resolution
+		if (isset($fileinfo['video']['resolution_x']) )
 		{
-            $width = $fileinfo['video']['resolution_x'];
-        }
-        if (isset($fileinfo['video']['resolution_y']) )
+			$width = $fileinfo['video']['resolution_x'];
+		}
+		if (isset($fileinfo['video']['resolution_y']) )
 		{
-            $height = $fileinfo['video']['resolution_y'];
-        }
-        if ( !isset($width) || !isset($height))
-	{
-            // If guess was unsuccessful, fallback to default 16/9 resolution
-            // This is the case for ogv video for example.
-            $width = $MAX_WIDTH;
-            $height = intval( 9 * ($width / 16 ));
-        }
-    }
+			$height = $fileinfo['video']['resolution_y'];
+		}
+		if ( !isset($width) || !isset($height))
+		{
+			// If guess was unsuccessful, fallback to default 16/9 resolution
+			// This is the case for ogv video for example.
+			$width = $MAX_WIDTH;
+			$height = intval( 9 * ($width / 16 ));
+		}
+	}
 	else // Not a supported video format or an image
 	{
 		return $content;
 	}
 
-    // Resize if video is too large
-    if ( $width > $MAX_WIDTH )
-    {
-        //$height = intval($height * ($MAX_WIDTH / $width));
-        //$width  = $MAX_WIDTH;
+	// Resize if video is too large
+	if ( $width > $MAX_WIDTH )
+	{
+		//$height = intval($height * ($MAX_WIDTH / $width));
+		//$width  = $MAX_WIDTH;
 		$height = intval($height / 2);
 		$width = intval($width / 2);
-    }
+	}
 
-    // Slideshow : The video needs to be launch automatically in
-    // slideshow mode. The refresh of the page is set to the
-    // duration of the video.
-    $AUTOPLAY = '';
-    if ( $page['slideshow'] )
+	// Slideshow : The video needs to be launch automatically in
+	// slideshow mode. The refresh of the page is set to the
+	// duration of the video.
+	$AUTOPLAY = '';
+	if ( $page['slideshow'] )
 	{
-        $refresh = $fileinfo['playtime_seconds'];
-        $AUTOPLAY = 'play';
-    }
+		$refresh = $fileinfo['playtime_seconds'];
+		$AUTOPLAY = 'play';
+	}
 
-    // Load parameter, fallback to blue monday if unset
-    $skin = isset($conf['videojs_skin']) ? $conf['videojs_skin'] : 'vjs-default-skin';
+	// Load parameter, fallback to blue monday if unset
+	$skin = isset($conf['videojs_skin']) ? $conf['videojs_skin'] : 'vjs-default-skin';
 
-    // Select the template
-    $template->set_filenames(
+	// Select the template
+	$template->set_filenames(
 		array('vjs_content' => dirname(__FILE__)."/template/vjs-player.tpl")
-    );
+	);
 
 	$parts = pathinfo($picture['current']['element_url']);
 	$thumb = get_gallery_home_url() . $parts['dirname'] . "/thumbnail/TN-" . $parts['filename'] . ".jpg";
 	//print $thumb;
 
-    // Assign the template variables
-    // We use here the piwigo's get_gallery_home_url function to build
-    // the full URL as suggested by videojs for flash fallback compatibility
-    $template->assign(
-        array(
-            'VIDEOJS_MEDIA_URL'	=> embellish_url(get_gallery_home_url() . $picture['current']['element_url']),
+	// Assign the template variables
+	// We use here the piwigo's get_gallery_home_url function to build
+	// the full URL as suggested by videojs for flash fallback compatibility
+	$template->assign(
+		array(
+			'VIDEOJS_MEDIA_URL'	=> embellish_url(get_gallery_home_url() . $picture['current']['element_url']),
 			'VIDEOJS_THUMB_URL'	=> $thumb,
-            'VIDEOJS_PATH'     => VIDEOJS_PATH,
-            'VIDEOJS_FULLPATH' => realpath(dirname(__FILE__)),
-            'WIDTH'            => $width,
-            'HEIGHT'           => $height,
-            'TYPE'             => $extension,
-            'AUTOPLAY'         => $AUTOPLAY,
+			'VIDEOJS_PATH'     => VIDEOJS_PATH,
+			'VIDEOJS_FULLPATH' => realpath(dirname(__FILE__)),
+			'WIDTH'            => $width,
+			'HEIGHT'           => $height,
+			'TYPE'             => $extension,
+			'AUTOPLAY'         => $AUTOPLAY,
 			'VJSSKIN'          => $skin,
-        )
-    );
+		)
+	);
 
-    // Return the rendered html
-    $vjs_content = $template->parse('vjs_content', true);
-    return $vjs_content;
+	// Return the rendered html
+	$vjs_content = $template->parse('vjs_content', true);
+	return $vjs_content;
 }
 
 function get_mimetype_icon ($location, $element_info)
 {
-    if ( empty( $element_info['tn_ext'] ) ) {
-        $extension = strtolower(get_extension($element_info['path']));
-        if ( $extension == 'webm') {
-            $location= 'plugins/'
-                       . basename(dirname(__FILE__))
-                       . '/mimetypes/webm.png';
-        }
-        $location= 'plugins/'
-                   . basename(dirname(__FILE__))
-                   . '/mimetypes/' . $extension . '.png';
-    }
-    return $location;
+	if ( empty( $element_info['tn_ext'] ) ) {
+		$extension = strtolower(get_extension($element_info['path']));
+		if ( $extension == 'webm') {
+			$location= 'plugins/'
+				. basename(dirname(__FILE__))
+				. '/mimetypes/webm.png';
+		}
+			$location= 'plugins/'
+				. basename(dirname(__FILE__))
+				. '/mimetypes/' . $extension . '.png';
+	}
+	return $location;
 }
 
 ?>
