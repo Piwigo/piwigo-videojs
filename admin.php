d5f88118fd28ab23c0d59bc718f73323e067e7e0
xbgmsharp <xbgmsharp@gmail.com>
2012-06-26 17:39:47 +0200

    Add: additional settings in Admin panel

--- admin.php
+++ admin.php
@@ -1,40 +1,84 @@
 <?php
-// Chech whether we are indeed included by Piwigo.
+// Check whether we are indeed included by Piwigo.
 if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+// Setup plugin Language
+load_language('plugin.lang', LLGBO_PATH);
 
 // Fetch the template.
-global $template, $conf;
+global $template, $conf, $lang;
 
 // Load parameter
-$skin = $conf['videojs_skin'];
+$skin = $conf['vjs_skin'];
+$preload = $conf['vjs_preload'];
+$control = $conf['vjs_controls'];
+$autoplay = $conf['vjs_autoplay'];
+$loop = $conf['vjs_loop'];
+$max_width = $conf['max_width'];
 
 // Available skins
 $available_skins = array(
-    'vjs-default-skin' => 'default',
-    'tubecss' => 'tubecss',
+	'vjs-default-skin' => 'default',
+	'tubecss' => 'tubecss',
+);
+
+// Available preload value
+$available_preload = array(
+	'auto' => 'Auto',
+	'none' => 'None',
+);
+
+// Available width value
+$available_width = array(
+	'480' => 'EDTV: (720x480) ie: 480p',
+	'720' => 'HDReady: (1280x720) ie: 720p',
+	'1080' => 'FullHD: (1920x1080) ie: 1080p',
 );
 
 // Update conf if submitted in admin site
-if (isset($_POST['submit']) && !empty($_POST['skin'])) {
-    $query = 'UPDATE ' . CONFIG_TABLE . 
-             ' SET value="' . $_POST['skin'] . '" 
-              WHERE param="videojs_skin"';
-    pwg_query($query);
-    // keep this selected in the admin form 
-    $skin = $_POST['skin'];
+if (isset($_POST['submit']) && !empty($_POST['skin']))
+{
+	$query = "UPDATE ". CONFIG_TABLE SET value='". $_POST['skin'] . "' WHERE param='vjs_skin'";
+	pwg_query($query);
+	$query = "UPDATE ". CONFIG_TABLE SET value='". $_POST['preload'] . "' WHERE param='vjs_preload'";
+	pwg_query($query);
+	$query = "UPDATE ". CONFIG_TABLE SET value='". $_POST['vjs_controls'] . "' WHERE param='vjs_controls'";
+	pwg_query($query);
+	$query = "UPDATE ". CONFIG_TABLE SET value='". $_POST['vjs_autoplay'] . "' WHERE param='vjs_autoplay'";
+	pwg_query($query);
+	$query = "UPDATE ". CONFIG_TABLE SET value='". $_POST['vjs_loop'] . "' WHERE param='vjs_loop'";
+	pwg_query($query);
+	$query = "UPDATE ". CONFIG_TABLE SET value='". $_POST['max_width'] . "' WHERE param='max_width'";
+	pwg_query($query);
+
+	// keep the value in the admin form
+	$skin = $_POST['skin'];
+	$preload = $_POST['preload'];
+	$control = $_POST['control'];
+	$autoplay = $_POST['autoplay'];
+	$loop = $_POST['loop'];
+	$max_width = $_POST['max_width'];
 }
 
 // 
-$template->assign(array(
-    'SELECTED_SKIN'   => $skin,
-    'AVAILABLE_SKINS' => $available_skins,
-));
+$template->assign(
+	array(
+		'SELECTED_SKIN'   => $skin,
+		'AVAILABLE_SKINS' => $available_skins,
+		'SELECTED_PRELOAD'  => $preload,
+		'AVAILABLE_PRELOAD' => $available_preload,
+		'SELECTED_WIDTH'  => $max_width,
+		'AVAILABLE_WIDTH' => $available_width,
+		'CONTROLS' => $control,
+		'AUTOPLAY' => $autoplay,
+		'LOOP' => $loop,
+	)
+);
 
 // Add our template to the global template
 $template->set_filenames(
-  array(
-    'plugin_admin_content' => dirname(__FILE__).'/admin.tpl'
-  )         
+	array(
+		'plugin_admin_content' => dirname(__FILE__).'/admin.tpl'
+	) 
 );
 
 // Assign the template contents to ADMIN_CONTENT
--- admin.tpl
+++ admin.tpl
@@ -1,15 +1,47 @@
-<!-- Show the title of the plugin -->
-<div class="titlePage">
-  <h2>videojs</h2>
-</div>
+<div class="infos" style="text-align:left;padding-left:45px;font-size:14px;font-weight:bold;">{$VJS_INFO|@translate}</div>
+ <h2> {'Title'|@translate} <br> {$VJS_VERSION}</h2>
+<p>{'Howto'|@translate}</p>
+<p>{'Howto1'|@translate}</p>
+<p>{'Howto2'|@translate}</p>
 
 <form method="post" action="" class="properties">
-  <fieldset>
-    <legend>videojs preferences</legend>
-    <label>Skin : </label>
-    <select name="skin">
-      {html_options options=$AVAILABLE_SKINS selected=$SELECTED_SKIN}
-    </select>
-    <input class="submit" type="submit" value="{'Submit'|@translate}" name="submit"/>
-  </fieldset>
+	<fieldset>
+		<legend>{pref|@translate}</legend>
+		<ul style="font-size:1em;font-weight:normal;margin-right:30%;text-align:right">
+			<li>
+				<label>{skin|@translate} : </label>
+				<select name="vjs_skin">
+					{html_options options=$AVAILABLE_SKINS selected=$SELECTED_SKIN}
+				</select>
+			</li>
+			<li>
+				<label>{preload|@translate} : </label>
+				<select name="vjs_preload">
+					{html_options options=$AVAILABLE_PRELOAD selected=$SELECTED_PRELOAD}
+				</select>
+			</li>
+			<li>
+				<label>{controls|@translate} : </label>
+				{html_radios name='vjs_controls' values='true,false'|@explode output='Yes,No'|@explode|translate selected=$CONTROLS}
+			</li>
+			<li>
+				<label>{autoplay|@translate} : </label>
+				{html_radios name='vjs_autoplay' values='true,false'|@explode output='Yes,No'|@explode|translate selected=$AUTOPLAY}
+			</li>
+			<li>
+				<label>{loop|@translate} : </label>
+				{html_radios name='vjs_loop' values='true,false'|@explode output='Yes,No'|@explode|translate selected=$LOOP}
+			</li>
+		</ul>
+		<legend>{pref2|@translate}</legend>
+		<ul style="font-size:1em;font-weight:normal;margin-right:30%;text-align:right">
+			<li>
+				<label>{width|@translate} : </label>
+				<select name="max_width">
+					{html_options options=$AVAILABLE_WIDTH selected=$SELECTED_WIDTH}
+				</select>
+			</li>
+		<ul>
+		<input class="submit" type="submit" value="{'Submit'|@translate}" name="submit"/>
+	</fieldset>
 </form>
