6a14da65797cee004ff404432658b51b12dbbeda
xbgmsharp <xbgmsharp@gmail.com>
2013-06-02 20:55:02 +0900

    Update language

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -1,15 +1,21 @@
 <?php
-$lang['submit'] = 'Submit';
 
-$lang['pref'] = 'HTML5 video tag settings';
-$lang['pref2'] = 'Plugin settings';
+$lang['HTML5'] = 'HTML5 video tag settings';
+$lang['PRELOAD'] = 'Preload';
+$lang['PRELOAD_DESC'] = 'The preload attribute informs the browser whether or not the video data should begin downloading as soon as the video tag is loaded.';
+$lang['CONTROLS'] = 'Controls';
+$lang['CONTROLS_DESC'] = 'The controls option sets whether or not the player has controls that the user can interact with';
+$lang['AUTOPLAY'] = 'Autoplay';
+$lang['AUTOPLAY_DESC'] = 'If autoplay is true, the video will start playing as soon as page is loaded (without any interaction from the user). NOT SUPPORTED BY APPLE iOS DEVICES.';
+$lang['LOOP'] = 'Loop';
+$lang['LOOP_DESC'] = 'The loop attribute causes the video to start over as soon as it ends.';
 
-$lang['skin'] = 'Skin';
-$lang['customcss'] = 'Custom CSS';
-$lang['preload'] = 'Preload';
-$lang['controls'] = 'Controls';
-$lang['autoplay'] = 'Autoplay';
-$lang['loop'] = 'Loop';
-$lang['width'] = 'Max Width';
+$lang['PLUGIN'] = 'Plugin settings';
+$lang['SKIN'] = 'Skin';
+$lang['SKIN_DESC'] = 'Select the style you want to apply to the player.';
+$lang['CUSTOMCSS'] = 'Custom CSS';
+$lang['CUSTOMCSS_DESC'] = 'Custom CSS to copy paste from the VideoJS website.';
+$lang['WIDTH'] = 'Max Width';
+$lang['WIDTH_DESC'] = 'The max width attribute sets the display width of the video, if higher it is divide by 2.';
 
 ?>
--- language/fr_FR/plugin.lang.php
+++ language/fr_FR/plugin.lang.php
@@ -1,15 +1,21 @@
 <?php
-$lang['submit'] = 'Valider';
 
-$lang['pref'] = 'HTML5 video tag préférences';
-$lang['pref2'] = 'Plugin préférences';
+$lang['HTML5'] = 'HTML5 video tag préférences';
+$lang['PRELOAD'] = 'Preload';
+$lang['PRELOAD_DESC'] = 'The preload attribute informs the browser whether or not the video data should begin downloading as soon as the video tag is loaded.';
+$lang['CONTROLS'] = 'Controls';
+$lang['CONTROLS_DESC'] = 'The controls option sets whether or not the player has controls that the user can interact with.';
+$lang['AUTOPLAY'] = 'Autoplay';
+$lang['AUTOPLAY_DESC'] = 'If autoplay is true, the video will start playing as soon as page is loaded (without any interaction from the user). NOT SUPPORTED BY APPLE iOS DEVICES.';
+$lang['LOOP'] = 'Loop';
+$lang['LOOP_DESC'] = 'The loop attribute causes the video to start over as soon as it ends.';
 
-$lang['skin'] = 'Skin';
-$lang['customcss'] = 'Custom CSS';
-$lang['preload'] = 'Preload';
-$lang['controls'] = 'Controls';
-$lang['autoplay'] = 'Autoplay';
-$lang['loop'] = 'Loop';
-$lang['width'] = 'Max Width';
+$lang['PLUGIN'] = 'Plugin préférences';
+$lang['SKIN'] = 'Style';
+$lang['SKIN_DESC'] = 'Selectionner un style a appliquer au lecteur.';
+$lang['CUSTOMCSS'] = 'Custom CSS';
+$lang['CUSTOMCSS_DESC'] = 'Custom CSS to copy paste from the VideoJS website.';
+$lang['WIDTH'] = 'Max Width';
+$lang['WIDTH_DESC'] = 'The max width attribute sets the display width of the video. if higher it is divide by 2.';
 
 ?>
