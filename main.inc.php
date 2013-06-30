795c3bdc45012c416c0575d00a2fa49ce567b8dd
xbgmsharp <xbgmsharp@gmail.com>
2013-06-30 10:43:43 +0900

    Bump version to 1.0.1

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: VideoJS
-Version: 1.0.0
+Version: 1.0.1
 Description: videojs integration for piwigo
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=610
 Author: xbmgsharp
