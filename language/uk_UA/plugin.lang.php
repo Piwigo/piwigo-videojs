c161fed776b94dfc059b303aa62eb13fd673675d
xbgmsharp <xbgmsharp@gmail.com>
2013-10-19 16:43:21 +0900

    Update/add new translation, thanks to the contributors

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['AUTOPLAY_DESC'] = 'Якщо Автоматичне програвання увімкнено, воно розпочнеться одразу по завантаженню сторінки (без взаємодії з користувачем). НЕ ПІДТРИМУЄТЬСЯ ПРИСТРОЯМИ З APPLE iOS.';
+$lang['CONTROLS'] = 'Елементи керування';
+$lang['CONTROLS_DESC'] = 'Ця опція дозволяє відобразити або приховати елементи керування, з котрими може взаємодіяти користувач.';
+$lang['CUSTOMCSS_DESC'] = 'Власний CSS для копіювання: <a href="http://www.videojs.com/" target="_blank">VideoJS website</a>.';
+$lang['HTML5'] = 'Налаштування відео-тегів для HTML5';
+$lang['PRELOAD_DESC'] = 'Цей атрибут вказує бравзеру чи потрібно попередньо завантажувати відео одразу при завантаженні відео-тегу.';
+$lang['SKIN_DESC'] = 'Виберіть стиль який ви хочете застоувати або <a href="http://designer.videojs.com/" target="_blank">створіть свій власний</a>.';
+$lang['WATERMARK_DESC'] = 'Відображає водяні знаки на відео';
+$lang['WIDTH'] = 'Максимальна ширина';
+$lang['WIDTH_DESC'] = 'Цей атрибут встановлює максимальну ширину відеозображення, до якої буде підігнане зображення при перевищенні вказаного розміру.';
+$lang['ZOOMROTATE'] = 'Збільшення/Поворот';
+$lang['ZOOMROTATE_DESC'] = 'Повернути та збільшити відео, якщо доступно (за наявності метаданих)';
+$lang['AUTOPLAY'] = 'Програти автоматично';
+$lang['CUSTOMCSS'] = 'Власний CSS';
+$lang['LOOP'] = 'Цикл';
+$lang['LOOP_DESC'] = 'Атрибут цикл грає одне й теж відео одразу по завершенню';
+$lang['PLUGIN'] = 'Налаштування додатку';
+$lang['PRELOAD'] = 'Попереднє завантаження';
+$lang['SKIN'] = 'Тема';
+$lang['THUMBNAILS'] = 'Мініатюри';
+$lang['THUMBNAILS_DESC'] = 'Відображати мініатюри над поступом';
+$lang['UPSCALE'] = 'Збільшення';
+$lang['UPSCALE_DESC'] = 'Якщо розмір зображення відео менший за максимальну ширину, воно буде збільшене до розміру максимальної ширини.';
+$lang['VIDEOJSPLUGIN'] = 'Додатки VideoJS';
+$lang['WATERMARK'] = 'Водяний знак';
+?>
---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['WIDTH_DESC'] = '最大寬度選項，設定視頻的顯示最大寬度，如視頻的尺寸更大，則其寬度將不超過設定寬度。';
+$lang['WATERMARK_DESC'] = '在視頻上顯示水印。';
+$lang['WIDTH'] = '最大寬度';
+$lang['ZOOMROTATE'] = '縮放 - 旋轉';
+$lang['ZOOMROTATE_DESC'] = '如可能，使用旋轉元數據對視頻進行旋轉和縮放。';
+$lang['AUTOPLAY'] = '自動播放';
+$lang['AUTOPLAY_DESC'] = '如啟用自動播放，視頻將在頁面加載完成後立即開始播放（不需接受用戶指令）。不被蘋果iOS設備支持​​。';
+$lang['CONTROLS'] = '控制';
+$lang['CONTROLS_DESC'] = '控制選項設定播放器是否提供用戶控制界面';
+$lang['CUSTOMCSS'] = 'CSS設定';
+$lang['CUSTOMCSS_DESC'] = '從<a href="http://www.videojs.com/" target="_blank">VideoJS 網站</a>拷貝粘貼代碼，以設定CSS。';
+$lang['HTML5'] = 'HTML5 視頻標籤(tag)設定';
+$lang['LOOP'] = '循環';
+$lang['LOOP_DESC'] = '循環選項設定，視頻結束後立即再次從頭播放。';
+$lang['PLUGIN'] = '插件設定';
+$lang['PRELOAD'] = '預加載';
+$lang['PRELOAD_DESC'] = '預加載選項，將告訴瀏覽器視頻數據，是否在視頻標籤加載後立即開始下載。';
+$lang['SKIN'] = '皮膚';
+$lang['SKIN_DESC'] = '選擇你想應用的播放器風格，或<a href="http://designer.videojs.com/" target="_blank">創建個性化的皮膚</a>。';
+$lang['THUMBNAILS'] = '縮略圖';
+$lang['THUMBNAILS_DESC'] = '在進度條上顯示縮略圖。';
+$lang['UPSCALE'] = '提升';
+$lang['UPSCALE_DESC'] = '如果視頻尺寸小於最大寬度，它會將視頻尺寸提升至最大寬度。';
+$lang['VIDEOJSPLUGIN'] = 'VideoJS 插件';
+$lang['WATERMARK'] = '水印';
+?>
