d449ede35376736ee998d4f715543149f519b1db
xbgmsharp <xbgmsharp@gmail.com>
2014-04-12 17:40:35 +0900

    Add/Update translation, thanks to the contributors

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ZOOMROTATE'] = 'زوم چرخشی';
+$lang['ZOOMROTATE_DESC'] = 'چرخش و زوم یک ویدیو در صورت امکان، استفاده از ابرداده چرخش.';
+$lang['SYNC_POSTEROVERLAY_DESC'] = 'درخواست همپوشانی در ایجاد پوستر';
+$lang['SYNC_POSTEROVERWRITE'] = 'بازنویسی پوستر های موجود';
+$lang['UPSCALE'] = 'مجلل';
+$lang['VIDEOJSPLUGIN'] = 'پلاگین جاوا اسکریپت';
+$lang['VOLUME'] = 'مقدار ';
+$lang['WATERMARK'] = 'ارم وعلایم ';
+$lang['WATERMARK_DESC'] = 'نشان دادن ارم روی ویدیو ';
+$lang['WIDTH'] = 'حداکثر عرض';
+$lang['SYNC_POSTEROVERLAY'] = 'اضافه کردن جلوه فیلم';
+$lang['SYNC_POSTER_DESC'] = ' ایجاد یک پوستر از این ویدئو را در موقعیت مشخص';
+$lang['SYNC_THUMBSEC'] = 'ایجاد یک تصویر بند انگشتی در هر ثانیه';
+$lang['SYNC_THUMBSEC_DESC'] = 'ایجاد یک تصویر بند انگشتی هر x ثانیه صورت گرفت.';
+$lang['SYNC_THUMBSIZE'] = 'اندازه تصویر بند انگشتی';
+$lang['SYNC_THUMBSIZE_DESC'] = 'ابعاد در پیکسل، کوچک نگه داشتن آن، به طور پیش فرض خوب است، یوتیوب استفاده 190x68.';
+$lang['SYNC_WARNINGS'] = 'هشدارها';
+$lang['THUMBNAILS'] = 'ریز عکسها';
+$lang['THUMBNAILS_DESC'] = 'نمایش تصاویر بر روی نوار پیشرفت کوچک.';
+$lang['AUTOPLAY'] = 'پخش خودکار';
+$lang['CONTROLS'] = 'کنترل گرها';
+$lang['CUSTOMCSS'] = 'استایل سفارشی';
+$lang['HTML5'] = 'تنظیم کردن برچسب های HTML5';
+$lang['LOOP'] = 'تکرار ';
+$lang['PLUGIN'] = 'تنظیمات پلاگین';
+$lang['PRELOAD'] = 'پیش نمایش';
+$lang['SKIN'] = 'پوسته';
+$lang['SYNC_ERRORS'] = 'خطا ها';
+$lang['SYNC_INFOS'] = 'جزئیات اطلاعات';
+$lang['SYNC_OUTPUT'] = 'فرمت خروجی';
+$lang['SYNC_POSTER'] = 'ایجاد پوستر موقعیت دوم';
+?>
