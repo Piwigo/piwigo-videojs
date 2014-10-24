4e65278696e0f93b66fbf0fe4d6175ba613ae6ce
xbgmsharp <xbgmsharp@gmail.com>
2014-10-24 19:40:01 +0900

    Add/Update translation, thanks to the contributors

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['SYNC_METADATA_DESC'] = 'Vil overskrive informasjonen i data basen med metadata fra videoen.';
+$lang['SYNC_OUTPUT'] = 'Utskrifts format';
+$lang['SYNC_OUTPUT_DESC'] = 'Velg utskrifts format for plakaten og miniatyrbilde. ';
+$lang['SYNC_POSTER'] = 'Lager en plakat i posisjon på sekunder';
+$lang['SYNC_INFOS'] = 'Detaljert informasjon';
+$lang['SYNC_ERRORS'] = 'Feil';
+$lang['SKIN'] = 'Stil';
+$lang['SKIN_DESC'] = 'Velg stilen du vil bruke på spilleren eller <a href="http://designer.videojs.com/" target="_blank">lage din egen Stil</a>.';
+$lang['CUSTOMCSS'] = 'Tilpasset CSS';
+$lang['LOOP_DESC'] = 'Sløyfens egenskaper får videoen til å starte på nytt så snart den slutter.';
+$lang['PLUGIN'] = 'Programtilleggs innstillinger';
+$lang['PRELOAD'] = 'Last på nytt';
+$lang['PRELOAD_DESC'] = 'Last på nytt egenskapen informerer nettleseren om ikke nedlastningen av videodata bør begynne så snart video taggen er lastet';
+$lang['LOOP'] = 'Sløyfe';
+$lang['HTML5'] = 'HTML5 video tagg innstillinger';
+$lang['CUSTOMCSS_DESC'] = 'Tilpasset CSS for å kopiere/lime inn fra <a href="http://www.videojs.com/" target="_blank">VideoJS website</a>.';
+$lang['CONTROLS'] = 'Kontrollelementer';
+$lang['CONTROLS_DESC'] = 'Kontrollelementenes alternativ angir hvorvidt eller ikke spilleren har kontroller som brukeren kan samhandle med';
+$lang['AUTOPLAY_DESC'] = 'Hvis autokjør er valgt, vil videoen begynn å spille så snart siden er lastet (uten interaksjon fra brukeren). STØTTES IKKE AV APPLE iOS ENHETER.';
+$lang['AUTOPLAY'] = 'Autokjør';
+$lang['SYNC_POSTEROVERLAY'] = 'Legg til film effekt';
+$lang['SYNC_POSTEROVERLAY_DESC'] = 'Påfør et overlegg på plakat skapelsen.';
+$lang['SYNC_POSTEROVERWRITE'] = 'Overskrive eksisterende plakater';
+$lang['SYNC_POSTEROVERWRITE_DESC'] = 'Overskrive eksisterende miniatyrbilder med nye. Hvis ikke avkrysset skal det bare kjøre for nylig tillagte video.';
