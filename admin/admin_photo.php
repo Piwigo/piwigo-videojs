8524905c4a1383bb346e614da4f82cd831e8b0ab
xbgmsharp <xbgmsharp@gmail.com>
2015-03-07 00:14:47 +0900

    Add Display all metadata read on the photo-edit page

--- admin/admin_photo.php
+++ admin/admin_photo.php
@@ -115,10 +115,10 @@ if (isset($exif))
 	// Import metadata into the DB
 	if (isset($_GET['sync_metadata']) and $_GET['sync_metadata'] == 1 and !empty($exif) and count($exif) > 0)
 	{
+		array_push( $page['infos'], ' metadata: '.count($exif)." ".implode(",", array_keys($exif)));
 		$dbfields = explode(",", "filesize,width,height,latitude,longitude,date_creation,rotation");
 		$query = "UPDATE ".IMAGES_TABLE." SET ".vjs_dbSet($dbfields, $exif).", `date_metadata_update`=CURDATE() WHERE `id`=".$_GET['image_id'].";";
 		pwg_query($query);
-		array_push( $page['infos'], ' metadata: '.implode(",", $dbfields));
 	}
 	// replace some value by human readable string
 	$exif['name'] = (string)$general->CompleteName;
