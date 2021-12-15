3ad2cf9778834f1eff1b1897be969bee772442b5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-15 07:06:33 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['SYNC_THUMBSIZE_DESC'] = 'Velikost v slikovnih pikah (px), naj bo majhna, privzeto je v redu, Youtube uporablja 190x68.';
+$lang['SYNC_THUMBSIZE'] = 'Velikost sličice';
+$lang['SYNC_THUMBSEC_DESC'] = 'Ustvarite sličico vsakih x sekund.';
+$lang['SYNC_THUMBSEC'] = 'Ustvarite sličico vsako sekundo';
+$lang['SYNC_POSTEROVERLAY_DESC'] = 'Uporabite prekrivanje na ustvarjanju plakata.';
+$lang['SYNC_POSTEROVERLAY'] = 'Dodajte filmski učinek';
+$lang['SYNC_OUTPUT_DESC'] = 'Izberite izhodno obliko za plakat in sličico.';
+$lang['SYNC_OUTPUT'] = 'Izhodni format';
+$lang['SYNC_POSTEROVERWRITE_DESC'] = 'Prepiši obstoječe sličice z novimi. Če počistite potrditveno polje, naj se izvaja samo za na novo dodane videoposnetke.';
+$lang['SYNC_POSTEROVERWRITE'] = 'Prepiši obstoječe plakate';
+$lang['SYNC_POSTER_DESC'] = 'Ustvarite plakat iz videoposnetka na določenem mestu.';
+$lang['SYNC_POSTER'] = 'Ustvarite plakat na položaju v drugem';
+$lang['SYNC_METADATA_DESC'] = 'Podatke v bazi podatkov bo prepisal z metapodatki iz videoposnetka.';
+$lang['SYNC_INFOS'] = 'Podrobne informacije';
+$lang['SYNC_WARNINGS'] = 'Opozorila';
+$lang['SYNC_ERRORS'] = 'Napake';
+$lang['RESOLUTION'] = 'Resolucija';
+$lang['RESOLUTION_DESC'] = 'Preklopnik resolucije.';
+$lang['WATERMARK'] = 'Vodni žig';
+$lang['WATERMARK_DESC'] = 'Prikaže vodni žig nad videoposnetkom.';
+$lang['THUMBNAILS'] = 'Sličice';
+$lang['THUMBNAILS_DESC'] = 'Prikaže sličice nad vrstico napredka.';
+$lang['ZOOMROTATE'] = 'ZoomRotate';
+$lang['ZOOMROTATE_DESC'] = 'Zasukajte in povečajte videoposnetek, če je primerno, uporabite metapodatke o vrtenju.';
+$lang['VIDEOJSPLUGIN'] = 'Vtičniki VideoJS';
+$lang['UPSCALE'] = 'Velikost';
+$lang['UPSCALE_DESC'] = 'Če je višina videoposnetka manjša od največje višine, bo velikost videoposnetka povečala na največjo višino.';
+$lang['HEIGHT'] = 'Največja višina';
+$lang['HEIGHT_DESC'] = 'Atribut maksimalne višine nastavi največjo višino prikaza videoposnetka. Če je višina videoposnetka večja od največje višine, bo velikost videoposnetka zmanjšala na največjo višino.';
+$lang['CUSTOMCSS'] = 'CSS po meri';
+$lang['CUSTOMCSS_DESC'] = 'CSS po meri za kopiranje prilepi s <a href="http://www.videojs.com/" target="_blank">spletnega mesta VideoJS</a>.';
+$lang['SKIN'] = 'Preobleka';
+$lang['SKIN_DESC'] = 'Izberite slog, ki ga želite uporabiti za predvajalnik, ali <a href="http://designer.videojs.com/" target="_blank">ustvarite svojo preobleko</a>.';
+$lang['PLUGIN'] = 'Nastavitve vtičnika';
+$lang['PLAYER_DESC'] = 'Izberite različico predvajalnika vjs.';
+$lang['METADATA_DESC'] = 'Opis metapodatkov';
+$lang['LANGUAGE_DESC'] = 'Izberite jezik predvajalnika.';
+$lang['LANGUAGE'] = 'Jezik';
+$lang['VOLUME'] = 'Glasnost';
+$lang['VOLUME_DESC'] = 'Z možnostjo glasnosti nastavite raven glasnosti. 0 je izklopljeno (utišano), 1,0 je popolnoma povečano, 0,5 je polovično povečano.';
+$lang['CONTROLS_DESC'] = 'Možnost kontrolnikov določa, ali ima predvajalnik kontrolnike, s katerimi lahko uporabnik komunicira.';
+$lang['LOOP'] = 'Zanka';
+$lang['LOOP_DESC'] = 'Atribut zanke povzroči, da se video začne znova, takoj ko se konča.';
+$lang['AUTOPLAY'] = 'Samodejno predvajanje';
+$lang['AUTOPLAY_DESC'] = 'Če je samodejno predvajanje res, se bo video začel predvajati takoj, ko se stran naloži (brez interakcije uporabnika). NAPRAVE APPLE iOS NI PODPRTO.';
+$lang['CONTROLS'] = 'Kontrole';
+$lang['PRELOAD'] = 'Prednalaganje';
+$lang['PRELOAD_DESC'] = 'Atribut prednalaganja obvesti brskalnik, ali naj se video podatki začnejo prenašati takoj, ko se naloži video oznaka.';
+$lang['HTML5'] = 'Nastavitve video oznak HTML5';
