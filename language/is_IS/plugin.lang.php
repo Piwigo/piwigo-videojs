2eb94e56d64be885bd95d43ce595e7d4bb91bef1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-25 08:27:39 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['UPSCALE'] = 'Skala upp';
+$lang['UPSCALE_DESC'] = 'Ef hæð myndbandsins er minni en hámarkshæð mun það stækka myndbandsstærðina í hámarkshæð.';
+$lang['VIDEOJSPLUGIN'] = 'VideoJS viðbætur';
+$lang['VOLUME'] = 'Hljóðstyrkur';
+$lang['VOLUME_DESC'] = 'Hljóðstyrksvalkosturinn stillir hljóðstyrkinn. Slökkt er á 0 (þaggað), 1,0 er alla leið upp, 0,5 er hálfa leið.';
+$lang['WATERMARK'] = 'Vatnsmerki';
+$lang['WATERMARK_DESC'] = 'Birtir vatnsmerki yfir allt myndbandið';
+$lang['ZOOMROTATE'] = 'ÞysjaSnúa';
+$lang['ZOOMROTATE_DESC'] = 'Snúðu og þysjaðu myndband ef við á, notaðu snúningslýsigögnin.';
+$lang['SKIN'] = 'Skinn';
+$lang['SKIN_DESC'] = 'Veldu stílinn sem þú vilt nota fyrir spilarann eða <a href="http://designer.videojs.com/" target="_blank">búðu til þitt eigið skinn</a>.';
+$lang['SYNC_ERRORS'] = 'Villur';
+$lang['SYNC_INFOS'] = 'Ítarlegar upplýsingar';
+$lang['SYNC_METADATA_DESC'] = 'Mun yfirskrifa upplýsingarnar í gagnagrunninum með lýsigögnum úr myndbandinu.';
+$lang['SYNC_OUTPUT'] = 'Úttakssnið';
+$lang['SYNC_OUTPUT_DESC'] = 'Veldu úttakssnið fyrir veggspjaldið og smámyndina.';
+$lang['SYNC_POSTER'] = 'Búðu til veggspjald í stöðu á sekúndu';
+$lang['SYNC_POSTEROVERLAY'] = 'Bættu við kvikmyndaáhrifum';
+$lang['SYNC_POSTEROVERLAY_DESC'] = 'Settu yfirlag á veggspjaldið.';
+$lang['SYNC_POSTEROVERWRITE'] = 'Yfirskrifaðu núverandi veggspjöld';
+$lang['SYNC_POSTEROVERWRITE_DESC'] = 'Yfirskrifaðu núverandi smámyndir með nýjum. Ef ekki hakað við ætti það aðeins að keyra fyrir nýlega viðbættu myndbandi.';
+$lang['SYNC_POSTER_DESC'] = 'Búðu til veggspjald úr myndbandinu á tilteknum stað.';
+$lang['SYNC_THUMBSEC'] = 'Búðu til smámynd á hverri sekúndu';
+$lang['SYNC_THUMBSEC_DESC'] = 'Búðu til smámynd á x sekúndna fresti.';
+$lang['SYNC_THUMBSIZE'] = 'Stærð smámyndarinnar';
+$lang['SYNC_THUMBSIZE_DESC'] = 'Stærð í pixlum, hafðu það lítið, sjálfgefið er í lagi, Youtube notar 190x68.';
+$lang['SYNC_WARNINGS'] = 'Aðvaranir';
+$lang['THUMBNAILS'] = 'Smámyndir';
+$lang['THUMBNAILS_DESC'] = 'Sýnir smámyndir yfir framvindustikunni.';
+$lang['AUTOPLAY'] = 'Sjálfvirk spilun';
+$lang['AUTOPLAY_DESC'] = 'Ef sjálfvirk spilun er valin, mun myndbandið byrja að spila um leið og síðunni er hlaðið (án nokkurrar samskipta frá notanda). EKKI STUÐNINGUR HJÁ APPLE iOS TÆKJUM.';
+$lang['CONTROLS'] = 'Stýringar';
+$lang['CONTROLS_DESC'] = 'Stýringarvalkosturinn stillir hvort spilarinn hafi stýringar sem notandinn getur haft samskipti við eða ekki';
+$lang['CUSTOMCSS'] = 'Sérsniðið CSS';
+$lang['CUSTOMCSS_DESC'] = 'Sérsniðið CSS til að afrita og líma úr <a href="http://www.videojs.com/" target="_blank">VideoJS vefsíða</a>.';
+$lang['HEIGHT'] = 'Hámarkshæð';
+$lang['HEIGHT_DESC'] = 'Eigindi hámarkshæðar stillir hámarksskjáhæð myndbandsins. Ef myndbandshæðin er stærri en hámarkshæð mun það minnka stærð myndbandsins í hámarkshæð.';
+$lang['HTML5'] = 'HTML5 myndbandamerkisstillingar';
+$lang['LANGUAGE'] = 'Tungumál';
+$lang['LANGUAGE_DESC'] = 'Velja tungumál spilara';
+$lang['LOOP'] = 'Lykkja';
+$lang['LOOP_DESC'] = 'Lykkjueiginleikinn veldur því að myndbandið byrjar aftur um leið og því lýkur.';
+$lang['METADATA_DESC'] = 'Lýsing lýsigagna';
+$lang['PLAYER_DESC'] = 'Velja útgáfu vjs spilara';
+$lang['PLUGIN'] = 'Stillingar viðbótar';
+$lang['PRELOAD'] = 'Forhlaða';
+$lang['PRELOAD_DESC'] = 'Forhleðslueiginleikinn upplýsir vafrann um hvort byrja eigi að hlaða niður myndbandsgögnum um leið og myndbandsmerkinu er hlaðið.';
+$lang['RESOLUTION'] = 'Upplausn';
+$lang['RESOLUTION_DESC'] = 'Upplausnarskiptir.';
