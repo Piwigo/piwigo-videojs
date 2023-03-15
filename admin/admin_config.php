Piwigo/piwigo-videojs

commit 0af268f5abf75066a7cd16293244973c5c78aa1b

Update admin photo to support multiple player

admin/admin_config.php
@@ -39,9 +39,11 @@ $available_skins = array(
 
 // Available player
 $available_players = array(
-	'vjs-player.tpl.v5' => 'VideoJS v5',
-	'vjs-player.tpl.v4' => 'VideoJS v4',
-	'html5-player.tpl' => 'Browser',
+	'vjs-7-player.tpl' => 'Video.js v7',
+	'vjs-6-player.tpl' => 'Video.js v6',
+	'vjs-5-player.tpl' => 'Video.js v5',
+	'vjs-4-player.tpl' => 'Video.js v4',
+	'html5-player.tpl' => 'Native Browser',
 );
 
 // Available preload value
@@ -52,7 +54,7 @@ $available_preload = array(
 
 // Available language value from the directory to be dynamic
 $available_languages = array('en');
-if ($handle = opendir(dirname(__FILE__).'/../video-js/lang/')) {
+if ($handle = opendir(dirname(__FILE__).'/../video-js-4/lang/')) {
 	while (false !== ($entry = readdir($handle))) {
 		if ($entry == '.' || $entry == '..') {
 			continue;

admin/admin_config.tpl
@@ -163,6 +163,12 @@ Refer to the <a href="https://github.com/xbgmsharp/piwigo-videojs/wiki" target="
 				<label><input type="radio" name="vjs_watermark" value="false" {if not $plugins.watermark}checked="checked"{/if}/> {'No'|@translate}</label>
 				<br/><small>{'WATERMARK_DESC'|@translate}</small>
 			</li>
+			<li>
+				<label><span class="property">{'RESOLUTION'|@translate} : </span></label>
+				<label><input type="radio" name="vjs_resolution" value="true" {if $plugins.resolution}checked="checked"{/if}/> {'Yes'|@translate}</label>
+				<label><input type="radio" name="vjs_resolution" value="false" {if not $plugins.resolution}checked="checked"{/if}/> {'No'|@translate}</label>
+				<br/><small>{'RESOLUTION_DESC'|@translate}</small>
+			</li>
 		</ul>
 	</fieldset>
 	<p>
@@ -176,7 +182,7 @@ function player_toggle()
 {
         var select = document.getElementById("vjs_player");
         var div = document.getElementById("player");
-	if (select.selectedIndex == 1) /* Only for VideoJS v4 */
+	if (select.selectedIndex == 3) /* Only for VideoJS v4 */
         {
                 div.removeAttribute("style");
         } else {

commit 0087ce527f910b0398f5b5cbe75c627075fadb3c

Complete VideoJS resolution plugin option
Fixes: 0af268f (Update admin photo to support multiple player)
Closes: #190

Suggested-by: Dechinta

admin/admin_config.php
@@ -93,6 +93,7 @@ if (isset($_POST['submit']) && !empty($_POST['vjs_skin']))
 							'zoomrotate'    => get_boolean($_POST['vjs_zoomrotate']),
 							'thumbnails'    => get_boolean($_POST['vjs_thumbnails']),
 							'watermark'     => get_boolean($_POST['vjs_watermark']),
+							'resolution'    => get_boolean($_POST['vjs_resolution']),
 						),
 		'player'	=> $_POST['vjs_player'],
 		'metadata'	=> get_boolean($_POST['vjs_metadata']),

maintain.inc.php
@@ -64,6 +64,7 @@ function plugin_install()
 					'zoomrotate'    => false,
 					'thumbnails'    => false,
 					'watermark'     => false,
+					'resolution'    => false,
 				),
 		'player'	=> 'vjs-5-player.tpl',
 		'metadata'	=> true,