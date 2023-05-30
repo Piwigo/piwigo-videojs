Piwigo/piwigo-videojs

commit 5e9b20928fbd4407b4e8d04339b28815424c751a

Add support for exiftool and ffprobe

include/ffprobe.php
@@ -1,0 +1,121 @@
+<?php
+/***********************************************
+* File      :   ffprobe.php
+* Project   :   piwigo-videojs
+* Descr     :   Handle metadata video parsing
+*
+* Created   :   21.07.2018
+*
+* Copyright 2012-2018 <xbgmsharp@gmail.com>
+*
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+************************************************/
+
+// Check whether we are indeed included by Piwigo.
+if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+
+try {
+putenv('LANG=en_US.UTF-8');
+$json = shell_exec($sync_options['ffprobe'] ." -hide_banner -loglevel fatal -show_error -show_format -show_streams -show_programs -show_chapters -show_private_data -print_format json \"". $filename."\"");
+if (!isset($json) or empty($json))
+	die("ffprobe error reading file. Is ffprobe install? Is ffprobe in path?<br/>Is the video accessible & readable, Try to run the command manually.<br/>". $sync_options['ffprobe'] ." -hide_banner -loglevel fatal -show_error -show_format -show_streams -print_format json '". $filename ."'");
+$output = json_decode($json, true);
+//print_r($output);
+} catch (Exception $e) {
+	die("ffprobe error reading file. Is ffprobe install? Is ffprobe in path?<br/>Is the video accessible & readable, Try to run the command manually.<br/>". $sync_options['ffprobe'] ." -hide_banner -loglevel fatal -show_error -show_format -show_streams -print_format json '". $filename ."'");
+}
+
+if ( !isset($output) and !is_array($output))
+{
+	$exif['error'] = "ffprobe error reading file: <br/>'". $filename."'";
+}
+
+if ( !isset($output['format']) and !isset($output['streams']))
+{
+	$exif['error'] = "ffprobe error reading output json: <br/>'". $output."'";
+}
+
+$general = $output['format'];
+if (isset($output['streams'][0]['codec_type']) and $output['streams'][0]['codec_type'] == 'video')
+{
+	$video = $output['streams'][0];
+	$audio = $output['streams'][1];
+} else {
+	$video = $output['streams'][1];
+	$audio = $output['streams'][0];
+}
+
+/* General */
+if (isset($general['size']))
+{
+	$exif['filesize'] = round($general['size']/1024);
+}
+if (isset($general['duration']))
+{
+	$exif['duration'] = round($general['duration']*1000, 0);
+	$exif['playtime_seconds'] = round($general['duration'], 0);
+}
+if (isset($general['bit_rate']))
+{
+	$exif['bitrate'] = (string)$general['bit_rate'];
+}
+if (isset($general['tags']['creation_time']))
+{
+	$exif['date_creation'] = date('Y-m-d H:i:s', strtotime((string)$general['tags']['creation_time']));
+}
+if (isset($general['tags']['location']))
+{
+	$gps = (string)$general['tags']['location'];
+	$value = preg_split('/(\+|\-)/', $gps, -1, PREG_SPLIT_DELIM_CAPTURE);
+	$exif['latitude'] = $value[1].$value[2];
+	$exif['longitude'] = $value[3].$value[4];
+}
+if (isset($general['tags']['major_brand']))
+{
+	$exif['codecid'] = $general['tags']['major_brand'];
+}
+
+/* Video */
+if (isset($video['width']))
+{
+	$exif['width'] = (string)$video['width'];
+}
+if (isset($video['height']))
+{
+	$exif['height'] = (string)$video['height'];
+}
+if (isset($video['tags']['rotate']) and (int)$video['tags']['rotate'] != 0)
+{
+	include_once(PHPWG_ROOT_PATH.'admin/include/image.class.php');
+	$rotation_code = pwg_image::get_rotation_code_from_angle((int)$video['tags']['rotate']);
+	$exif['rotation'] = $rotation_code;
+}
+if (isset($video['tags']['avg_frame_rate']))
+{
+	$exif['frame_rate'] = $video['tags']['avg_frame_rate'];
+}
+
+/* Audio */
+if (isset($audio['channels']))
+{
+	$exif['channel'] = (string)$audio['channels'];
+}
+if (isset($audio['sample_rate']))
+{
+	$exif['sampling_rate'] = (string)$audio['sample_rate'];
+}
+
+?>

include/exiftool.php
@@ -1,0 +1,117 @@
+<?php
+/***********************************************
+* File      :   exiftool.php
+* Project   :   piwigo-videojs
+* Descr     :   Handle general video parsing
+*
+* Created   :   02.04.2015
+*
+* Copyright 2012-2018 <xbgmsharp@gmail.com>
+*
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+************************************************/
+
+// Check whether we are indeed included by Piwigo.
+if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+
+try {
+putenv('LANG=en_US.UTF-8');
+$json = shell_exec($sync_options['exiftool'] ." -struct -n -json \"". $filename."\"");
+if (!isset($json) or empty($json))
+	die("Exiftool error reading file. Is Exiftool install? Is Exiftool in path?<br/>Is the video accessible & readable, Try to run the command manually.<br/>". $sync_options['exiftool'] ." -json '". $filename ."'");
+$output = json_decode($json, true);
+//print_r($output);
+} catch (Exception $e) {
+	die("Exiftool error reading file. Is Exiftool install? Is Exiftool in path?<br/>Is the video accessible & readable, Try to run the command manually.<br/>". $sync_options['exiftool'] ." -json '". $filename ."'");
+}
+
+if ( !isset($output) and !is_array($output))
+{
+	$exif['error'] = "Exiftool error reading file: <br/>'". $filename."'";
+}
+
+$general = $output[0];
+
+/* General */
+if (isset($general['FileSize']))
+{
+	$exif['filesize'] = round($general['FileSize']/1024);
+}
+if (isset($general['Duration']))
+{
+	$exif['duration'] = round($general['Duration']*1000, 0);
+}
+if (isset($general['TrackDuration']))
+{
+	$exif['playtime_seconds'] = round($general['TrackDuration'], 0);
+}
+if (isset($general['AvgBitrate']))
+{
+	$exif['bitrate'] = (string)$general['AvgBitrate'];
+}
+if (isset($general['MediaCreateDate']))
+{
+	$exif['date_creation'] = date('Y-m-d H:i:s', strtotime((string)$general['MediaCreateDate']));
+}
+if (isset($general['GPSLatitude']) and isset($general['GPSLongitude']))
+{
+	$exif['latitude'] = $general['GPSLatitude'];
+	$exif['longitude'] = $general['GPSLongitude'];
+}
+if (isset($general['MajorBrand']))
+{
+	$exif['codecid'] = $general['MajorBrand'];
+}
+if (isset($general['MIMEType']))
+{
+	$exif['type'] = $general['MIMEType'];
+}
+if (isset($general['FileType']))
+{
+	$exif['format'] = $general['FileType'];
+}
+
+/* Video */
+if (isset($general['ImageWidth']))
+{
+	$exif['width'] = (string)$general['ImageWidth'];
+}
+if (isset($general['ImageHeight']))
+{
+	$exif['height'] = (string)$general['ImageHeight'];
+}
+if (isset($general['Rotation']) and (int)$general['Rotation'] != 0)
+{
+	include_once(PHPWG_ROOT_PATH.'admin/include/image.class.php');
+	$rotation_code = pwg_image::get_rotation_code_from_angle((int)$general['Rotation']);
+	$exif['rotation'] = $rotation_code;
+}
+if (isset($general['VideoFrameRate']))
+{
+	$exif['frame_rate'] = $general['VideoFrameRate'];
+}
+
+/* Audio */
+if (isset($general['AudioChannels']))
+{
+	$exif['channel'] = (string)$general['AudioChannels'];
+}
+if (isset($general['AudioSampleRate']))
+{
+	$exif['sampling_rate'] = (string)$general['AudioSampleRate'];
+}
+
+?>

commit 4f81c4a2f84d0a56c3d7b44fdc920dfe8a353a8c

Fix longitude sync using ffprobe
ffprobe may provide the longitude value as:

  "
    location        : +60.1568+024.8577/
  "

Hence, this will result in a piwigo-videojs longitude value of:

  "
    longitude: +024.8577/
  "

That will lead to the following fatal error:

  "
    Fatal error: Uncaught mysqli_sql_exception: Data truncated for column 'longitude' at row 1 in /app/www/public/include/dblayer/functions_mysqli.inc.php:132 Stack trace: #0 /app/www/public/include/dblayer/functions_mysqli.inc.php(132): mysqli->query() #1 /config/www/plugins/piwigo-videojs/admin/admin_photo.php(126): pwg_query() #2 /app/www/public/admin/plugin.php(53): include_once('...') #3 /app/www/public/admin.php(345): include('...') #4 {main} thrown in /app/www/public/include/dblayer/functions_mysqli.inc.php on line 132
  "

Fixes: 5e9b209 (Add support for exiftool and ffprobe)
Closes: #167

Suggested-by: chrud

include/ffprobe.php
@@ -97,7 +97,7 @@ if (isset($general['tags']['location']))
 	$gps = (string)$general['tags']['location'];
 	$value = preg_split('/(\+|\-|\/)/', $gps, -1, PREG_SPLIT_DELIM_CAPTURE);
 	$exif['latitude'] = $value[1].$value[2];
-	$exif['longitude'] = $value[3].$value[4];
+	$exif['longitude'] = $value[3].rtrim($value[4],'/');
 }
 if (isset($general['tags']['major_brand']))
 {